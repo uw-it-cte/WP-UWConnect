d42b160f85e17d78ed121e3b6fc5b4ca790eec6a
ALi Haugh <haughar@hotmail.com>
2015-03-03 13:17:42 -0800

    Add metabox with Short Description field.

--- services.php
+++ services.php
@@ -30,4 +30,58 @@ function service_post_type() {
 }
 
 add_action('init', 'service_post_type');
+
+add_action('add_meta_boxes', 'service_info');
+
+function service_info() {
+    add_meta_box('service_form', 'Service Details', 'service_content', 'service', 'normal', 'default');
+}
+
+function service_content($object, $box) {
+    wp_nonce_field(basename(__FILE__), 'service_details_nonce'); ?>
+
+    <p>
+        <label for='description'><?php _e('Short Description', 'services'); ?></label>
+        <input type='text' name='short-description' id='short-description' value="<?php echo esc_attr(get_post_meta($object->ID, 'short-description', true)); ?>" size="90" />
+    </p>
+<?php }
+
+add_action('save_post', 'save_service_form', 10, 2);
+
+function save_service_form($post_id, $post) {
+
+    if (!verify_save('service_details_nonce', $post_id))
+        return $post_id;
+
+    update_service($post_id, 'service_form', 'short-description', sanitize_post_field('short-description', $_POST['short-description'], $post_id, 'display'));
+}
+
+function verify_save ($nonce_name, $post_id) {
+    if (!isset($_POST[$nonce_name])) {
+        return false;
+    }
+    if (!wp_verify_nonce($_POST[$nonce_name], basename(__FILE__))) {
+        return false;
+    }
+    if (!current_user_can('edit_page', $post_id)) {
+        return false;
+    }
+
+    return true;
+}
+
+function update_service($post_id, $service_form, $detail_name) {
+    $new_meta_value = (isset($_POST[$detail_name]) ? sanitize_post_field($detail_name, $_POST[$detail_name], $post_id, 'display'):'');
+
+    $meta_key = $detail_name;
+
+    $meta_value = get_post_meta($post_id, $meta_key, true);
+
+    if ($new_meta_value && '' == $meta_value)
+        add_post_meta($post_id, $meta_key, $new_meta_value, true);
+    elseif ($new_meta_value && $new_meta_value != $meta_value)
+        update_post_meta($post_id, $meta_key, $new_meta_value);
+    elseif ('' == $new_meta_value && $meta_value)
+        delete_post_meta($post_id, $meta_key, $meta_value);
+}
 ?>
