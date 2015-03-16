25966ac35713f5c665867a734e00ac7cdbd63c4f
Cole Bessee <cbessee@uw.edu>
2015-03-16 11:27:07 -0700

    add namespace to meta keys retrieved in service template

--- single-service.php
+++ single-service.php
@@ -13,28 +13,28 @@ get_header(); ?>
                 $id = $post->ID;?>
                 <h1><?php the_title(); ?></h1>
                 <h2>Service Description:</h2>
-                <p><?php echo get_post_meta($id, 'description', true); ?></p>
+                <p><?php echo get_post_meta($id, 'uwc-description', true); ?></p>
                 <h2>Service Options:</h2>
-                <p><?php echo get_post_meta($id, 'options_text', true); ?></p>
+                <p><?php echo get_post_meta($id, 'uwc-options_text', true); ?></p>
                 <h2>Eligibility:</h2>
-                <p><?php echo get_post_meta($id, 'eligibility', true); ?></p>
+                <p><?php echo get_post_meta($id, 'uwc-eligibility', true); ?></p>
                 <h2>How to Order:</h2>
-                <p><?php echo get_post_meta($id, 'ordering', true); ?></p>
+                <p><?php echo get_post_meta($id, 'uwc-ordering', true); ?></p>
                 <h2>Availability:</h2>
-                <p><?php echo get_post_meta($id, 'availability', true); ?></p>
+                <p><?php echo get_post_meta($id, 'uwc-availability', true); ?></p>
                 <h2>Price:</h2>
-                <p><?php echo get_post_meta($id, 'price', true); ?></p>
+                <p><?php echo get_post_meta($id, 'uwc-price', true); ?></p>
                 <h2>Additional Information:</h2>
-                <p><?php echo get_post_meta($id, 'additional_info', true); ?></p>
+                <p><?php echo get_post_meta($id, 'uwc-additional_info', true); ?></p>
                 <h2>Support Information:</h2>
-                <p><?php echo get_post_meta($id, 'support_info', true); ?></p>
+                <p><?php echo get_post_meta($id, 'uwc-support_info', true); ?></p>
                 <h2>Contact for More Information:</h2>
-                <p><?php echo get_post_meta($id, 'customer_ref', true); ?></p>
+                <p><?php echo get_post_meta($id, 'uwc-customer_ref', true); ?></p>
                 <h2>Maintainance:</h2>
                 <h3>Last Review Date:</h3>
-                <p><?php echo get_post_meta($id, 'last_review', true); ?></p>
+                <p><?php echo get_post_meta($id, 'uwc-last_review', true); ?></p>
                 <h3>Next Review Date:</h3>
-                <p><?php echo get_post_meta($id, 'next_review', true); ?></p>
+                <p><?php echo get_post_meta($id, 'uwc-next_review', true); ?></p>
                 <?php endwhile; ?>
             </div> <!-- #primary -->
         </div> <!-- #content -->
