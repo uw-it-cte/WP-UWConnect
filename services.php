c5e016858374ee4655f8a5edcf98d040ebfb91fc
Craig M. Stimmel <cstimmel@uw.edu>
2015-04-08 14:55:50 -0700

    $term doesn't exist until inside the for loop.

--- services.php
+++ services.php
@@ -238,9 +238,9 @@ function taxonomy_list_shortcode($atts) {
     $taxonomy = get_taxonomy($tax);
     $output = '<h4 class="tax-head">' . $taxonomy->labels->name . '</h4><ul>';
     $siteurl = site_url();
-    $url = $siteurl . '/' . $tax .  '/' . $term->slug;
+    $url = $siteurl . '/' . $tax;
     foreach ($terms as $term) {
-        $output .= '<a href="' . $url . '"><li>' . $term->name . '</li></a>';
+        $output .= '<a href="' . $url . '/' . $term->slug .'"><li>' . $term->name . '</li></a>';
     }
     $output .= '</ul>';
     return $output;
