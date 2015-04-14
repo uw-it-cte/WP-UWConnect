f7f03e8b03a85a43ab4974013d4a81716065f439
Cole Bessee <cbessee@uw.edu>
2015-04-14 16:22:05 -0700

    Change sidebar size for service catalog pages, REFS ITC-774

--- service-home.php
+++ service-home.php
@@ -3,12 +3,12 @@ get_header();
 ?>
     <div id='main-content' class='row main-content'>
         <div id='content' class='site-content it_container' role='main'>
-            <div id='secondary' class='col-lg-2 col-md-2 hidden-sm hidden-xs' role='complementary'>
+            <div id='secondary' class='col-lg-3 col-md-3 hidden-sm hidden-xs' role='complementary'>
                 <div id='sidebar' role='navigation' aria-label='Sidebar Menu'>
                     <?php dynamic_sidebar('Service-Catalog-Sidebar'); ?>
                 </div> <!-- #sidebar -->
             </div> <!-- #secondary -->
-            <div id='primary' class='col-xs-12 col-sm-12 col-md-10 col-lg-10'>
+            <div id='primary' class='col-xs-12 col-sm-12 col-md-9 col-lg-9'>
                 <?php service_breadcrumbs(); ?>
                 <div style='margin-left:25px;'>
                 <?php while (have_posts()) : the_post();
